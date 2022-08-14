015e70b366ce8641a55cdd9f4a3079f79366aa3e
Shadabimran07 <md.shadabimran.aiml25@heritageit.edu.in>
2022-08-15 00:44:49 +0530

    created account login logout

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import { Drawer, Button, Divider, Alert } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/firebase';
+import EditableInput from './EditableInput';
+import ProviderBlock from './ProviderBlock';
 
-const Dashboard = () => {
-  return <div>Hello</div>;
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+
+  const onSave = async newData => {
+    const userNicknameRef = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
+
+    try {
+      await userNicknameRef.set(newData);
+
+      Alert.success('Nickname has been updated', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+        <ProviderBlock />
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          lable={<h6 className="mb-2"> Nickname</h6>}
+        />
+      </Drawer.Body>
+
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dashboard;
